732dcc4b3742b29ec270e877cad7b9d8c7e873f4
mannukaushik <mannukaushikece@gmail.com>
2018-05-18 14:53:43 +0530

    Committing the api call functionality

---
+++ src/actions/resourceFactory.js
@@ -0,0 +1,35 @@
+export const FETCH_PRODUCTS_BEGIN = 'FETCH_PRODUCTS_BEGIN';
+export const FETCH_PRODUCTS_SUCCESS = 'FETCH_PRODUCTS_SUCCESS';
+export const FETCH_PRODUCTS_FAILURE = 'FETCH_PRODUCTS_FAILURE';
+
+export function resourceFactory() {
+
+    return dispatch => {
+        dispatch(fetchProductsBegin());
+        return fetch('http://localhost:8080/resonance/v1/products').then(handleErrors)
+            .then(res => res.json)
+            .then(json => {
+                dispatch(fetchProductsSuccess(json.products));
+                return json.products;
+            }).catch(error => dispatch(fetchProductsError(error)));
+    };
+}
+function handleErrors(response) {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
+export const fetchProductsBegin = () => ({
+    type: FETCH_PRODUCTS_BEGIN
+});
+
+export const fetchProductsSuccess = products => ({
+    type: FETCH_PRODUCTS_SUCCESS,
+    payload: { products }
+});
+
+export const fetchProductsError = error => ({
+    type: FETCH_PRODUCTS_FAILURE,
+    payload: { error }
+});
--- src/components/CustomNavBar.js
+++ src/components/CustomNavBar.js
@@ -7,43 +7,60 @@ import { increment, incrementAsync, decrement, decrementAsync } from '../reducer
 import '../css/styles.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, NavDropdown, MenuItem } from '../../node_modules/react-bootstrap/dist/react-bootstrap.min.js';
+import { resourceFactory } from '../actions/resourceFactory';
 
-const CustomNavbar = () => (
-  <div>
-  <br/>
-   <Navbar className="nav-color" role="navigation">
-    <Navbar.Header>
-      <Navbar.Brand className="nav-color-text">
-        <a href="/">Resonance</a>
-      </Navbar.Brand>
-    </Navbar.Header>
+export class CustomNavbar extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(resourceFactory());
+  }
+  render() {
+    
+    const { error, loading, products } = this.props;
+    console.log(this.props);
 
-    <Nav pullRight>
-      <NavDropdown eventKey={1} title="Home Audio" id="basic-nav-dropdown">
-      <MenuItem><Link to='/soundbars'className="nullifyLink"><MenuItem eventKey={1.1} className="nullifyLink">Sound Bars</MenuItem></Link></MenuItem>
-        <MenuItem eventKey={1.2}>Home Theater Systems</MenuItem>
-        <MenuItem eventKey={1.3}>Tower Speakers</MenuItem>
-      </NavDropdown>
-      <NavDropdown eventKey={2} title="Support" id="basic-nav-dropdown">
-        <MenuItem eventKey={2.1}>Contact Us</MenuItem>
-        <MenuItem divider />
-        <MenuItem eventKey={2.2}>Why Buy Direct ?</MenuItem>
-      </NavDropdown>
-    </Nav>
+    if (loading) {
+      return <div>Loading...</div>;
+    }
 
-  </Navbar>
-  </div>
-)
+      return (
+
+        <div>
+          <br />
+          <Navbar className="nav-color" role="navigation">
+            <Navbar.Header>
+              <Navbar.Brand className="nav-color-text">
+                <a href="/">Resonance</a>
+              </Navbar.Brand>
+            </Navbar.Header>
+
+            <Nav pullRight>
+              <NavDropdown eventKey={1} title="Home Audio" id="basic-nav-dropdown">
+                <MenuItem><Link to='/hometheaters/soundbars' className="nullifyLink" onClick={this.props.dispatch(resourceFactory())}><MenuItem eventKey={1.1} className="nullifyLink">Sound Bars</MenuItem></Link></MenuItem>
+                <MenuItem eventKey={1.2}>Home Theater Systems</MenuItem>
+                <MenuItem eventKey={1.3}>Tower Speakers</MenuItem>
+              </NavDropdown>
+              <NavDropdown eventKey={2} title="Support" id="basic-nav-dropdown">
+                <MenuItem eventKey={2.1}>Contact Us</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={2.2}>Why Buy Direct ?</MenuItem>
+              </NavDropdown>
+            </Nav>
+
+          </Navbar>
+        </div>
+      );
+  }
+}
 
 
-const mapStateToProps = state => ({ count: state.counter.count, isIncrementing: state.counter.isIncrementing, isDecrementing: state.counter.isDecrementing })
+const mapStateToProps = state => ({ items: state.products, loading: state.products, error: state.products });
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-  increment,
-  incrementAsync,
-  decrement,
-  decrementAsync,
-  changePage: () => push('/Linkbout-us')
-}, dispatch)
+// const mapDispatchToProps = dispatch => bindActionCreators({
+//   increment,
+//   incrementAsync,
+//   decrement,
+//   decrementAsync,
+//   changePage: () => push('/Linkbout-us')
+// }, dispatch)
 
-export default connect(mapStateToProps, mapDispatchToProps)(CustomNavbar);
+export default connect(mapStateToProps)(CustomNavbar);
