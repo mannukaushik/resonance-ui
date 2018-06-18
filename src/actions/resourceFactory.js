00af06187162f798c8398c18bbac2e4c5cfff159
mannukaushikece@gmail.com <mannukaushikece@gmail.com>
2018-06-18 07:03:37 +0530

    adding proper json

--- src/actions/resourceFactory.js
+++ src/actions/resourceFactory.js
@@ -3,14 +3,15 @@ import data from '../utils/metamodels/soundbars';
 export const FETCH_PRODUCTS_BEGIN = 'FETCH_PRODUCTS_BEGIN';
 export const FETCH_PRODUCTS_SUCCESS = 'FETCH_PRODUCTS_SUCCESS';
 export const FETCH_PRODUCTS_FAILURE = 'FETCH_PRODUCTS_FAILURE';
-
+var summaryItems;
 export function resourceFactory(apiUrl) {
-
+  
   return dispatch => {
     dispatch(fetchProductsBegin());
     return fetch(apiUrl, { method: 'OPTIONS' })
       .then(handleErrors)
       .then(res => res.json())
+      .then(insertSummary)
       .then(json => {
         dispatch(fetchProductsSuccess(json._options.links.map((links) => {
           if ((links.method === 'GET') && (data.metamodel.rel === 'search')) {
@@ -18,11 +19,11 @@ export function resourceFactory(apiUrl) {
               .then(handleErrors)
               .then(res => res.json())
               .then(json => {
-                 dispatch(fetchProductsSuccess(json._links.item.map((item)=>item.summary))
+                 dispatch(fetchProductsSuccess(json._links.item.map((item)=>{item.summary}))
               )
           }
         )}})));
-        return json._links.item;
+        return json;
       })
       .catch(error => dispatch(fetchProductsError(error)));
   };
@@ -33,6 +34,11 @@ function handleErrors(response) {
     throw Error(response.statusText);
   }
   return response;
+}
+function insertSummary(json){
+  json._options.links.map((fields)=>{
+    summaryItems=fields.schema.properties 
+  });
 }
 export const fetchProductsBegin = () => ({ type: FETCH_PRODUCTS_BEGIN });
 
