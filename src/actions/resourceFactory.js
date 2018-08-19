f5f4f5eb59b298ecd4b50f6ad5f271ee1500e2ff
Mannu Kaushik <mannukaushik>
2018-08-19 15:53:59 +0530

    schema set incomplete changes

--- src/actions/resourceFactory.js
+++ src/actions/resourceFactory.js
@@ -6,31 +6,44 @@ export const FETCH_PRODUCTS_FAILURE = 'FETCH_PRODUCTS_FAILURE';
 export const schemaSet = new Set();
 
 export function resourceFactory(apiUrl) {
-
+  var summaryObject = {};
   return dispatch => {
     dispatch(fetchProductsBegin());
-    return fetch(apiUrl, {method: 'OPTIONS'})
+    return fetch(apiUrl, {
+      method: 'OPTIONS'
+    })
       .then(handleErrors)
       .then(res => res.json())
       .then(insertSummary)
       .then(json => {
-        json
-          ._options
-          .links
-          .map((links) => {
-            if ((links.method === 'GET') && (data.metamodel.rel === 'search')) {
-              fetch(links.href, {method: links.method})
-                .then(handleErrors)
-                .then(res => res.json())
-                .then(json => {
-                  dispatch(fetchProductsSuccess(json._links.item.map((item) => {
-                    return item.summary
-                  })))
-                  
-                })
-            }
-          });
-       
+        json._options.links.map((links) => {
+          if ((links.method === 'GET') && (data.metamodel.rel === 'search')) {
+            fetch(links.href, {
+              method: links.method
+            })
+              .then(handleErrors)
+              .then(res => res.json())
+              .then(json => {
+                dispatch(fetchProductsSuccess(json._links.item.map((item) => {
+                  const keys = Object.keys(item.summary);
+                  keys.map((key) => {
+
+                    if (schemaSet.has(key)) {
+                      summaryObject.key = keys.map((key) => {
+                        item.summary.filter(
+                          (data) => { return data.key === key }
+                        );
+                        return key;
+                      })
+                    }
+                  });
+
+                })))
+
+              })
+          }
+        });
+
       })
       .catch(error => dispatch(fetchProductsError(error)));
   };
@@ -42,43 +55,43 @@ function handleErrors(response) {
   }
   return response;
 }
+
 function insertSummary(json) {
-  json
-    ._options
-    .links
-    .map((fields) => {
-      if (fields.rel === data.metamodel.rel) {
-        schemaSet.add(fields.schema.properties);
-      }
-    });
+  json._options.links.map((fields) => {
+    if (fields.rel === data.metamodel.rel) {
+      schemaSet.add(fields.schema.properties);
+    }
+  });
 
   const setIterator = schemaSet[Symbol.iterator]();
-  const values = setIterator
-    .next()
-    .value;
+  const values = setIterator.next().value;
   var keys = Object.keys(values);
   schemaSet.clear();
 
-  data
-    .metamodel
-    .properties
-    .map((metamodelNames) => {
-      keys
-        .forEach(function (key) {
-          if (key === metamodelNames.name) {
-            schemaSet.add(metamodelNames.name);
-          }
-        });
+  data.metamodel.properties.map((metamodelNames) => {
+    keys.forEach(function (key) {
+      if (key === metamodelNames.name) {
+        schemaSet.add(metamodelNames.name);
+      }
     });
+  });
   return json;
 }
-export const fetchProductsBegin = () => ({type: FETCH_PRODUCTS_BEGIN});
+export const fetchProductsBegin = () => ({
+  type: FETCH_PRODUCTS_BEGIN
+});
 
 export function fetchProductsSuccess(products) {
 
-  return {type: FETCH_PRODUCTS_SUCCESS, payload: products}
+  return {
+    type: FETCH_PRODUCTS_SUCCESS,
+    payload: products
+  }
 }
 
-export const fetchProductsError = error => ({type: FETCH_PRODUCTS_FAILURE, payload: {
+export const fetchProductsError = error => ({
+  type: FETCH_PRODUCTS_FAILURE,
+  payload: {
     error
-  }});
+  }
+});
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import CustomNavBar from '../components/CustomNavBar';
 import ImageLoader from '../components/ImageLoader';
-import soundBars from '../components/soundBars';
+import soundBars from '../components/SoundBars';
 
 const App = () => (
   <div>
