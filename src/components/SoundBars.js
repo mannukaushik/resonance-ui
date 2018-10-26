2a56076139ad397b678e02646806a14b4582b749
mannu <mannukaushik@github.com>
2018-10-26 13:52:38 +0530

    Added changes for react table
    React table changes for soundbars

--- src/components/SoundBars.js
+++ src/components/SoundBars.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import ReactTable from 'react-table';
+import "react-table/react-table.css";
+import appConf from '../utils/metamodels/config.json';
+import '../css/styles.css';
 
 export class soundBars extends React.Component {
 
     render() {
         const { item } = this.props;
-        
-        if(item.length===0 || item[0]===undefined){
+       
+        if (item.length === 0 || item[0] === undefined) {
             return <span>Loading...</span>
         }
         return (
             <div>
-               {item.map((itemSummary)=> <div><span>{itemSummary.modelName}</span><br/><span>{itemSummary.price}</span><br/></div>)}
+                <ReactTable data={item} columns={[
+                    {
+                        Header: "SoundBars",
+                        columns: [
+                            {     
+                                id: "imgSrc",                           
+                                Cell: ()=><img src ={appConf.config.publicUrl + item[0].imgSrc} alt={item.modelName} class="thumbnail"/>
+                            },
+                            {
+                                Header:"Product Name",
+                                accessor: "modelName"
+                                
+                            },
+                            {
+                                Header:"Price",
+                                accessor : "price"
+                            }
+                        ]
+
+                    }]}
+                    defaultPageSize={10}
+                    className="-striped -highlight"
+                    ></ReactTable>
             </div>
         );
     }
